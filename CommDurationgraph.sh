668df048aaa447625be32bc4757a60107592835e
Jon Connolly <joncon@uw.edu>
2016-08-30 17:51:21 -0700

    Added all 4 stations with unique colors.

--- CommDurationgraph.sh
+++ CommDurationgraph.sh
@@ -18,10 +18,24 @@ GPRINT:CORE_Comm_Duration0:LAST:"Last\:%2.2lf" \
 GPRINT:CORE_Comm_Duration0:AVERAGE:"Avg\:%2.2lf" \
 GPRINT:CORE_Comm_Duration0:MAX:"Max\:%2.2lf" \
 GPRINT:CORE_Comm_Duration0:MIN:"Min\:%2.2lf\n" \
-DEF:CD1=/var/www/rrd/TEST_mppt.rrd:Comm_Duration:AVERAGE \
-CDEF:TEST_Comm_Duration1=CD1,1,*  \
-LINE1:TEST_Comm_Duration1#00ff00:"TEST_mppt Communications Duration in Seconds" \
-GPRINT:TEST_Comm_Duration1:LAST:"Last\:%2.2lf" \
-GPRINT:TEST_Comm_Duration1:AVERAGE:"Avg\:%2.2lf" \
-GPRINT:TEST_Comm_Duration1:MAX:"Max\:%2.2lf" \
-GPRINT:TEST_Comm_Duration1:MIN:"Min\:%2.2lf\n" 
+DEF:CD1=/var/www/rrd/LCCR_mppt.rrd:Comm_Duration:AVERAGE \
+CDEF:LCCR_Comm_Duration1=CD1,1,*  \
+LINE1:LCCR_Comm_Duration1#00ff00:"LCCR_mppt Communications Duration in Seconds" \
+GPRINT:LCCR_Comm_Duration1:LAST:"Last\:%2.2lf" \
+GPRINT:LCCR_Comm_Duration1:AVERAGE:"Avg\:%2.2lf" \
+GPRINT:LCCR_Comm_Duration1:MAX:"Max\:%2.2lf" \
+GPRINT:LCCR_Comm_Duration1:MIN:"Min\:%2.2lf\n" \
+DEF:CD2=/var/www/rrd/MARC_mppt.rrd:Comm_Duration:AVERAGE \
+CDEF:MARC_Comm_Duration2=CD2,1,*  \
+LINE1:MARC_Comm_Duration2#0000ff:"MARC_mppt Communications Duration in Seconds" \
+GPRINT:MARC_Comm_Duration2:LAST:"Last\:%2.2lf" \
+GPRINT:MARC_Comm_Duration2:AVERAGE:"Avg\:%2.2lf" \
+GPRINT:MARC_Comm_Duration2:MAX:"Max\:%2.2lf" \
+GPRINT:MARC_Comm_Duration2:MIN:"Min\:%2.2lf\n" \
+DEF:CD3=/var/www/rrd/TEST_mppt.rrd:Comm_Duration:AVERAGE \
+CDEF:TEST_Comm_Duration3=CD3,1,*  \
+LINE1:TEST_Comm_Duration3#ffff00:"TEST_mppt Communications Duration in Seconds" \
+GPRINT:TEST_Comm_Duration3:LAST:"Last\:%2.2lf" \
+GPRINT:TEST_Comm_Duration3:AVERAGE:"Avg\:%2.2lf" \
+GPRINT:TEST_Comm_Duration3:MAX:"Max\:%2.2lf" \
+GPRINT:TEST_Comm_Duration3:MIN:"Min\:%2.2lf\n"

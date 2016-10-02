7d3563ab4deffea29cd065e2d80631d7b3bddde8
Jon Connolly <joncon@uw.edu>
2016-10-02 13:28:56 -0700

    changed battery temp line types from 1 to 2, thiker for easy reading.

--- tgraph.sh
+++ tgraph.sh
@@ -23,12 +23,12 @@ DEF:T_amb=/var/www/rrd/${sta}_mppt.rrd:T_amb:AVERAGE \
 DEF:T_batt=/var/www/rrd/${sta}_mppt.rrd:T_batt:AVERAGE \
 CDEF:Temp_Amb=T_amb,1,*  \
 CDEF:Temp_Batt=T_batt,1,* \
-LINE1:Temp_Amb#ff0000:"Ambient Temperature in Celcius" \
+LINE2:Temp_Amb#ff0000:"Ambient Temperature in Celcius" \
 GPRINT:Temp_Amb:LAST:"Last\:%2.2lf" \
 GPRINT:Temp_Amb:AVERAGE:"Avg\:%2.2lf" \
 GPRINT:Temp_Amb:MAX:"Max\:%2.2lf" \
 GPRINT:Temp_Amb:MIN:"Min\:%2.2lf\n" \
-LINE1:Temp_Batt#00ff00:"Battery Temperature in Celcius" \
+LINE2:Temp_Batt#00ff00:"Battery Temperature in Celcius" \
 GPRINT:Temp_Batt:LAST:"Last\:%2.2lf" \
 GPRINT:Temp_Batt:AVERAGE:"Avg\:%2.2lf" \
 GPRINT:Temp_Batt:MAX:"Max\:%2.2lf" \

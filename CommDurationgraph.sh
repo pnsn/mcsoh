2b959d6dfb9b1f96c6c437382c95eda3735bcb29
Jon Connolly <joncon@uw.edu>
2016-11-07 14:30:58 -0800

    Added FISH CommDuration to comm's graph in purple

--- CommDurationgraph.sh
+++ CommDurationgraph.sh
@@ -38,4 +38,11 @@ LINE1:YACT_Comm_Duration3#ffff00:"YACT_mppt Communications Duration in Seconds" 
 GPRINT:YACT_Comm_Duration3:LAST:"Last\:%2.2lf" \
 GPRINT:YACT_Comm_Duration3:AVERAGE:"Avg\:%2.2lf" \
 GPRINT:YACT_Comm_Duration3:MAX:"Max\:%2.2lf" \
-GPRINT:YACT_Comm_Duration3:MIN:"Min\:%2.2lf\n"
+GPRINT:YACT_Comm_Duration3:MIN:"Min\:%2.2lf\n" \
+DEF:CD4=/var/www/rrd/FISH_mppt.rrd:Comm_Duration:AVERAGE \
+CDEF:FISH_Comm_Duration4=CD4,1,*  \
+LINE1:FISH_Comm_Duration4#990066:"FISH_mppt Communications Duration in Seconds" \
+GPRINT:FISH_Comm_Duration4:LAST:"Last\:%2.2lf" \
+GPRINT:FISH_Comm_Duration4:AVERAGE:"Avg\:%2.2lf" \
+GPRINT:FISH_Comm_Duration4:MAX:"Max\:%2.2lf" \
+GPRINT:FISH_Comm_Duration4:MIN:"Min\:%2.2lf\n"

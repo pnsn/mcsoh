a33f21233accd63f044965d8ad56fdd4a292fc72
Jon Connolly <joncon@uw.edu>
2016-12-18 15:52:41 -0800

    Added BROK and BAND to index.html and make_images.sh

--- make_images.sh
+++ make_images.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #./CommDurationgraph.sh
 /bin/bash -l -c "/home/deploy/mcsoh/CommDurationgraph.sh"
-for sta in CORE FISH LCCR MARC TEST YACT; do
+for sta in BAND BROK CORE FISH LCCR MARC TEST YACT; do
 echo $sta 
 
 /bin/bash -l -c "/home/deploy/mcsoh/cmgraph.sh ${sta}"
